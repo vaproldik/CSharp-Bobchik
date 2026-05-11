d57d2fb0467fae3e87424ea5867f5ed3d088d22a
vaproldik <162791536+vaproldik@users.noreply.github.com>
2026-05-11 13:28:24 +0300

    Day 19. Платформа разработки Веб-приложений ASP.NET

---
+++ Day19/ExpenseTracker/Controllers/ExpensesController.cs
@@ -0,0 +1,47 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private static List<Expense> expenses = new List<Expense>()
+        {
+            new Expense { Id = 1, Amount = 500, Category = "Еда", Date = DateTime.Now },
+            new Expense { Id = 2, Amount = 1200, Category = "Транспорт", Date = DateTime.Now },
+            new Expense { Id = 3, Amount = 3000, Category = "Развлечения", Date = DateTime.Now }
+        };
+
+        public IActionResult Index()
+        {
+            return View(expenses);
+        }
+
+        [HttpGet]
+        [Route("Expenses/Filter/{category}")]
+        public IActionResult Filter(string category)
+        {
+            var filtered = expenses
+                .Where(e => e.Category != null &&
+                            e.Category.ToLower() == category.ToLower())
+                .ToList();
+
+            return View("Index", filtered);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Expense expense)
+        {
+            expense.Id = expenses.Count + 1;
+            expenses.Add(expense);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Day19/ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string? Category { get; set; }
+        public DateTime Date { get; set; }
+
+
+    }
+}
